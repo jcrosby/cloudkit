fce2414b20f246c04164080e2d86cd69785e91ba
Jon Crosby <jon@kaboomerang.com>
2008-05-18 23:21:59 -0700

    Added rake tasks for easy publication of major, minor, and patch releases for desktop apps. [#8 state:resolved] [#9 state:resolved] [#10 state:resolved]

--- lib/cloudkit/air/application_descriptor.rb
+++ lib/cloudkit/air/application_descriptor.rb
@@ -2,7 +2,7 @@ module CloudKit
   module AIR
     class ApplicationDescriptor
       require 'rexml/document'
-      attr_reader :name, :version, :path, :directory
+      attr_reader :name, :version, :path, :directory, :filename, :app_id
       
       def initialize(path)
         @path = path
@@ -10,6 +10,8 @@ module CloudKit
         doc = REXML::Document.new(File.open(path, 'r').read)
         @name = REXML::XPath.first(doc, '//name').children[0]
         @version = REXML::XPath.first(doc, '//version').children[0]
+        @filename = REXML::XPath.first(doc, '//filename').children[0]
+        @app_id = REXML::XPath.first(doc, '//id').children[0]
       end
     end
   end
