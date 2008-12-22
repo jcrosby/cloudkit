bb12ca21524fe8d107b1fe76342813d2f602edb8
Jon Crosby <jon@joncrosby.me>
2008-12-22 02:48:35 -0800

    tweak default landing page

--- lib/cloudkit/rack/builder.rb
+++ lib/cloudkit/rack/builder.rb
@@ -51,10 +51,67 @@ doc = <<HTML
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-  <title>CloudKit</title>
+  <title>CloudKit via cURL</title>
+  <style type="text/css">
+    body {
+      font-family: 'Helvetica', 'Arial', san-serif;
+      font-size: 15px;
+      margin: 0;
+      padding: 0;
+      color: #222222;
+    }
+    h1 {
+      font-family: 'Helvetica Neue', 'Helvetica', 'Arial', san-serif;
+      font-size: 73px;
+      font-weight: bold;
+      line-height: 28px;
+      margin: 20px 0px 20px 0px;
+    }
+    .wrapper {
+      width: 500px;
+      margin: 0 auto;
+      clear: both;
+    }
+    p {
+      margin-top: 0px;
+      line-height: 1.5em;
+    }
+    #header {
+      background-color: #ffffcc;
+      display: block;
+      padding: 2px 0;
+      margin: 35px 0px 10px 0px;
+      border-top: 1px solid #ffcc66;
+      border-bottom: 1px solid #ffcc66;
+    }
+    a {
+      color: #6b8df2;
+      text-decoration: none;
+    }
+    .meta {
+      padding: 7px 7px 7px 7px;
+      background-color: #ffccff;
+      border-top: 1px solid #cc99ff;
+      border-bottom: 1px solid #cc99ff;
+      font-size: 14px;
+      display: block;
+      margin: 10px 0px 10px 0px;
+    }
+  </style>
 </head>
 <body>
-<h1>CloudKit</h1>
+  <div id="header">
+    <div class="wrapper">
+      <h1>CloudKit</h1>
+    </div>
+  </div>
+  <div class="meta">
+    <p class="wrapper">
+      This page is appearing because you have not set up a default app in your
+      rackup file. To learn more about CloudKit, check out
+      <a href="http://getcloudkit.com">the site</a>.
+    </p>
+  </div>
 </body>
 </html>
 HTML
